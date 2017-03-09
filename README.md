e91e30f4b7be923d9a2ef54072b1a15519a0d1dd
Alejandro Niño <alejandro.nino@kairosds.com>
2017-03-09 15:33:09 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Nodejs SimpleServer
+==============
+
+> Tip: Check out [nodejs](https://nodejs.org/en/download/) and [npm](https://docs.npmjs.com/cli/install) are installed
+
+Description 
+---------------------
+Node.js simple server example
+
+Usage
+---------------------
+```sh
+run node server.js
+```
+Server will be listening [here](http://localhost:1337) 
+
+A [nodejs](https://nodejs.org) application.
+
---
+++ server.js
@@ -0,0 +1,65 @@
+// Require modules
+let http = require('http');
+let url = require('url');
+let path = require('path');
+let fs = require('fs');
+
+// Array of mime types
+let mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+// Create server
+http.createServer(function (req, res) {
+
+    let uri = url.parse(req.url).pathname;
+    let fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    let stats;
+
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('404 Not found');
+        res.end();
+        return;
+    }
+
+    // Check if file/directory
+    if (stats.isFile()) {
+        let mimeType = mimeTypes[path.extname(fileName).split('.')
+            .reverse()[0]];
+        res.writeHead(200, {
+            'Content-Type': mimeType
+        });
+        let fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, {
+            'Location': 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('500 Internal error');
+        res.end();
+    }
+
+}).listen(1337, '127.0.0.1');
+
+/*
+ * http.createServer(function (req, res){ res.writeHead(200, {'Content-type':
+ * 'text/plain'}); res.end('Hello world!'); }).listen(1337, '127.0.0.1');
+ */
+
+console.info('Server runing at http://localhost:1337');
